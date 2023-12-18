3bbe569a8aff8d1f78cae50bb1e22336a8d1337c
xnzvl <jakubnezval.jn@gmail.com>
2023-12-18 21:43:45 +0100

    core: Monster implementation

--- src/main/java/github/xnzvl/karak/mortals/monsters/Monster.java
+++ src/main/java/github/xnzvl/karak/mortals/monsters/Monster.java
@@ -3,6 +3,28 @@ package github.xnzvl.karak.mortals.monsters;
 import github.xnzvl.karak.items.Item;
 import github.xnzvl.karak.mortals.Mortal;
 
-public abstract class Monster implements Mortal {
-    // Item wouldDrop();
+import org.jetbrains.annotations.NotNull;
+
+public enum Monster implements Mortal {
+    ;
+
+    private final int power;
+    private final @NotNull Item drop;
+
+    Monster(
+            int power,
+            @NotNull Item drop
+    ) {
+        this.power = power;
+        this.drop = drop;
+    }
+
+    @Override
+    public int getPower() {
+        return this.power;
+    }
+
+    public Item getDrop() {
+        return this.drop;
+    }
 }
