aa6e7ad896ffdc72c653fc92c66f1f141582ff09
xnzvl <jakubnezval.jn@gmail.com>
2023-12-20 00:36:59 +0100

    core: adding monsters to Monster enum

--- src/main/java/github/xnzvl/karak/powerfuls/monsters/Monster.java
+++ src/main/java/github/xnzvl/karak/powerfuls/monsters/Monster.java
@@ -1,12 +1,23 @@
 package github.xnzvl.karak.powerfuls.monsters;
 
+import github.xnzvl.karak.items.Chest;
 import github.xnzvl.karak.items.Item;
+import github.xnzvl.karak.items.Key;
+import github.xnzvl.karak.items.Spell;
+import github.xnzvl.karak.items.Weapon;
 import github.xnzvl.karak.powerfuls.Powerful;
 
 import org.jetbrains.annotations.NotNull;
 
 public enum Monster implements Powerful {
-    ;
+    RAT            (5,  Weapon.DAGGER),
+    KEY_SKELETON   (8,  Key.getInstance()),
+    SWORD_SKELETON (9,  Weapon.SWORD),
+    AXE_SKELETON   (10, Weapon.AXE),
+    SPIDER         (8,  Spell.FOUNTAIN_TELEPORT),
+    MUMMY          (7,  Spell.FLAMING_SWORD),
+    GUARDIAN       (12, Chest.UNLOCKED_CHEST),
+    DRAGON         (15, Chest.SAPHIRE_CHEST);
 
     private final int power;
     private final @NotNull Item drop;
