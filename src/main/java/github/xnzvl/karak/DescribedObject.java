78f3f7f995401bf8e163305f0408da9b40941819
xnzvl <jakubnezval.jn@gmail.com>
2023-12-27 03:19:39 +0100

    docs(DescribedObject): adding documentation

--- src/main/java/github/xnzvl/karak/DescribedObject.java
+++ src/main/java/github/xnzvl/karak/DescribedObject.java
@@ -2,7 +2,25 @@ package github.xnzvl.karak;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Interface for convenient obtaining description of an object.
+ *
+ * @author Jakub Nezval
+ */
 public interface DescribedObject {
+    /**
+     * Method for obtaining the shortest possible description of the object.
+     * Returned string should contain only smaller units of words.
+     *
+     * @return short description of the object
+     */
     @NotNull String getTitle();
+
+    /**
+     * Method for obtaining the detailed description of the object.
+     * Careful! This method might return longer texts.
+     *
+     * @return detailed description of the object
+     */
     @NotNull String getDetails();
 }
