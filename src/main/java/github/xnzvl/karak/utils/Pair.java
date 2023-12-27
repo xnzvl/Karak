a163ef6d88375f79700cdf54631020035d092695
xnzvl <jakubnezval.jn@gmail.com>
2023-12-27 03:55:34 +0100

    core(Pair): docs & equals + hashCode

--- src/main/java/github/xnzvl/karak/utils/Pair.java
+++ src/main/java/github/xnzvl/karak/utils/Pair.java
@@ -1,7 +1,45 @@
 package github.xnzvl.karak.utils;
 
+import java.util.Objects;
+
+/**
+ * Record for pairing two values of any type together.
+ *
+ * @param x first value
+ * @param y second value
+ * @param <X> type of the first value
+ * @param <Y> type of the second value
+ *
+ * @author Jakub Nezval
+ */
 public record Pair<X, Y>(X x, Y y) {
-    public static <X, Y> Pair<X, Y> of(X x, Y y) {
-        return new Pair<>(x, y);
+    /**
+     * Sugar method for creating instances.
+     *
+     * @param xValue x value
+     * @param yValue y value
+     * @return new instance
+     * @param <X> X type
+     * @param <Y> Y type
+     */
+    public static <X, Y> Pair<X, Y> of(
+            X xValue,
+            Y yValue
+    ) {
+        return new Pair<>(xValue, yValue);
+    }
+
+    @Override
+    public boolean equals(
+            Object o
+    ) {
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(this.x, pair.x) && Objects.equals(this.y, pair.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
