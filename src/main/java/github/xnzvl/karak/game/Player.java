42cb043cf94335ae41164f3cb714b7c07bc1ad08
xnzvl <jakubnezval.jn@gmail.com>
2023-12-27 03:20:48 +0100

    core(Player): adopting interface renaming

--- src/main/java/github/xnzvl/karak/game/Player.java
+++ src/main/java/github/xnzvl/karak/game/Player.java
@@ -1,8 +1,9 @@
 package github.xnzvl.karak.game;
 
+import github.xnzvl.karak.DescribedObject;
 import github.xnzvl.karak.powerfuls.heroes.Hero;
 
-public interface Player {
+public interface Player extends DescribedObject {
     String getName();
     Hero getHero();
     void takeTurn();
