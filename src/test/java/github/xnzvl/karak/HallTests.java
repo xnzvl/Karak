b21c97841201779a865714d9537625c2c21aa147
xnzvl <jakubnezval.jn@gmail.com>
2023-12-28 23:42:19 +0100

    test(core/HallTests): proper tests for the first time!

--- src/test/java/github/xnzvl/karak/HallTests.java
+++ src/test/java/github/xnzvl/karak/HallTests.java
@@ -1,7 +1,7 @@
 package github.xnzvl.karak;
 
 import github.xnzvl.karak.tiles.Hall;
-import github.xnzvl.karak.tiles.Rotation;
+import github.xnzvl.karak.tiles.TileRotation;
 import github.xnzvl.karak.tiles.TileShape;
 import github.xnzvl.karak.utils.Pair;
 
@@ -9,37 +9,73 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HallTests {
-    private static Stream<Arguments> tileCoordsStream() {
-        return Stream.of(
-                Arguments.of(Pair.of(0,0)),
-                Arguments.of(Pair.of(3,7)),
-                Arguments.of(Pair.of(7,-3)),
-                Arguments.of(Pair.of(-3,-7)),
-                Arguments.of(Pair.of(-7,3))
+    private static final List<Pair<Integer, Integer>> variousCoords = List.of(
+            Pair.of(0,0),
+            Pair.of(3,7),
+            Pair.of(7,-3),
+            Pair.of(-3,-7),
+            Pair.of(-7,3)
+    );
+
+    private static Stream<Arguments> tileConfigurations() {
+        List<Arguments> configs = new ArrayList<>(
+                variousCoords.size() * TileShape.values().length * TileRotation.values().length
         );
+        for (var tileShape : TileShape.values()) {
+            for (var rotation : TileRotation.values()) {
+                for (var coords : variousCoords) {
+                    configs.add(Arguments.of(coords, tileShape, rotation));
+                }
+            }
+        }
+        return configs.stream();
+    }
+
+    private static Pair<Integer, Integer> clockwiseShift(
+            Pair<Integer, Integer> coordsToShift,
+            int howManyTimes
+    ) {
+        int x = coordsToShift.xValue();
+        int y = coordsToShift.yValue();
+        int tmp;
+
+        for (int i = 0; i < howManyTimes; i++) {
+            tmp = x;
+            x = y;
+            y = -tmp;
+        }
+
+        return Pair.of(x, y);
     }
 
     @ParameterizedTest
-    @MethodSource("tileCoordsStream")
-    void getAccessibleCoordinates_ShapeIRotationOne_isCorrect(
-            Pair<Integer, Integer> tileCoords
+    @MethodSource("tileConfigurations")
+    void getAccessibleCoordinates_allConfigurations_isCorrect(
+            Pair<Integer, Integer> coordinates,
+            TileShape shape,
+            TileRotation rotation
     ) {
-        Hall hall = new Hall(tileCoords, TileShape.I, Rotation.ONE);
+        Hall hall = new Hall(coordinates, shape, rotation);
 
-        var expected = List.of(
-                Pair.of(tileCoords.xValue(), tileCoords.yValue() + 1),
-                Pair.of(tileCoords.xValue(), tileCoords.yValue() - 1)
-        );
+        var expected = shape.getDoorsTo().stream()
+                .map(directions -> clockwiseShift(directions, rotation.getNumberOfShifts()))
+                .map(coords -> Pair.of(
+                        coordinates.xValue() + coords.xValue(),
+                        coordinates.yValue() + coords.yValue())
+                )
+                .toList();
 
         assertEquals(
-                expected,
-                hall.getAccessibleCoordinates()
+                new HashSet<>(expected),
+                new HashSet<>(hall.getAccessibleCoordinates())
         );
     }
 }
