60b8eb310c9cbb578e231ecb477144da1c39491c
xnzvl <jakubnezval.jn@gmail.com>
2023-12-28 15:13:35 +0100

    test(core/Hall): better structure of tests & better dependency

--- src/test/java/github/xnzvl/karak/HallTests.java
+++ src/test/java/github/xnzvl/karak/HallTests.java
@@ -5,20 +5,41 @@ import github.xnzvl.karak.tiles.Rotation;
 import github.xnzvl.karak.tiles.TileShape;
 import github.xnzvl.karak.utils.Pair;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HallTests {
-    @Test
-    void getAccessibleCoordinates_ShapeIRotationOne_isCorrect() {
-        var originHall = new Hall(Pair.of(0, 0), TileShape.I, Rotation.ONE);
+    private static Stream<Arguments> tileCoordsStream() {
+        return Stream.of(
+                Arguments.of(Pair.of(0,0)),
+                Arguments.of(Pair.of(3,7)),
+                Arguments.of(Pair.of(7,-3)),
+                Arguments.of(Pair.of(-3,-7)),
+                Arguments.of(Pair.of(-7,3))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("tileCoordsStream")
+    void getAccessibleCoordinates_ShapeIRotationOne_isCorrect(
+            Pair<Integer, Integer> tileCoords
+    ) {
+        Hall hall = new Hall(tileCoords, TileShape.I, Rotation.ONE);
+
+        var expected = List.of(
+                Pair.of(tileCoords.xValue(), tileCoords.yValue() + 1),
+                Pair.of(tileCoords.xValue(), tileCoords.yValue() - 1)
+        );
 
         assertEquals(
-                List.of(Pair.of(0, 1), Pair.of(0, -1)),
-                originHall.getAccessibleCoordinates()
+                expected,
+                hall.getAccessibleCoordinates()
         );
     }
 }
