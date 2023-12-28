a86105c6f782b344b69fd67b0b425cef18c25c62
xnzvl <jakubnezval.jn@gmail.com>
2023-12-28 02:24:37 +0100

    core(Tile): util enumerations docs & impl expanding

--- src/main/java/github/xnzvl/karak/tiles/Rotation.java
+++ src/main/java/github/xnzvl/karak/tiles/Rotation.java
@@ -1,8 +1,30 @@
 package github.xnzvl.karak.tiles;
 
+/**
+ * Enumeration for number of rotations that can {@link Tile} have.
+ *
+ * @author Jakub Nezval
+ */
 public enum Rotation {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST;
+    ONE   (0),
+    TWO   (1),
+    THREE (2),
+    FOUR  (3);
+
+    private final int numberOfShifts;
+
+    Rotation(
+            int numberOfShifts
+    ) {
+        this.numberOfShifts = numberOfShifts;
+    }
+
+    /**
+     * Get number of clockwise shifts. The number is always non-negative.
+     *
+     * @return number of clockwise shifts
+     */
+    public int getNumberOfShifts() {
+        return this.numberOfShifts;
+    }
 }
--- src/main/java/github/xnzvl/karak/tiles/TileShape.java
+++ src/main/java/github/xnzvl/karak/tiles/TileShape.java
@@ -1,8 +1,38 @@
 package github.xnzvl.karak.tiles;
 
+import github.xnzvl.karak.utils.Pair;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Enumeration for describing {@link Tile} shape.
+ *
+ * @author Jakub Nezval
+ */
 public enum TileShape {
-    I,
-    L,
-    T,
-    X;
+    I (List.of(Pair.of(0, 1), Pair.of(0, -1))),
+    L (List.of(Pair.of(0, 1), Pair.of(1, 0))),
+    T (List.of(Pair.of(0, 1), Pair.of(1, 0), Pair.of(0, -1))),
+    X (List.of(Pair.of(0, 1), Pair.of(1, 0), Pair.of(0, -1), Pair.of(-1, 0)));
+
+    private final @NotNull List<@NotNull Pair<@NotNull Integer, @NotNull Integer>> doorsTo;
+
+    TileShape(
+            @NotNull List<@NotNull Pair<@NotNull Integer, @NotNull Integer>> doorsTo
+    ) {
+        this.doorsTo = doorsTo;
+    }
+
+    /**
+     * Get directions that lead from a {@link Tile} center - Pair.of(0, 0).
+     * xValue and yValue of a direction pair are an element of set {-1, 0, 1}.
+     *
+     * @return collection of direction pairs
+     */
+    public @NotNull Collection<@NotNull Pair<@NotNull Integer, @NotNull Integer>> getDoorsTo() {
+        return this.doorsTo;
+    }
 }
