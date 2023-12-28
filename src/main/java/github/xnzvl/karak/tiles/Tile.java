c351c7b0ec3155ce3aab133549534bf253898299
xnzvl <jakubnezval.jn@gmail.com>
2023-12-28 02:00:52 +0100

    docs(Tile): short descriptions

--- src/main/java/github/xnzvl/karak/tiles/Tile.java
+++ src/main/java/github/xnzvl/karak/tiles/Tile.java
@@ -2,10 +2,33 @@ package github.xnzvl.karak.tiles;
 
 import github.xnzvl.karak.utils.Pair;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Collection;
 
+/**
+ * Interface that represents a game tile.
+ *
+ * @author Jakub Nezval
+ */
 public interface Tile {
-    Collection<Pair<Integer, Integer>> getAccessibleTiles();
-    TileShape getTileShape();
-    Rotation getRotation();
+    /**
+     * @return get coordinates of the tile
+     */
+    @NotNull Pair<@NotNull Integer, @NotNull Integer> getCoordinates();
+
+    /**
+     * @return collection of coordinates that are reachable from this tile
+     */
+    @NotNull Collection<@NotNull Pair<@NotNull Integer, @NotNull Integer>> getAccessibleCoordinates();
+
+    /**
+     * @return tile shape of the tile
+     */
+    @NotNull TileShape getTileShape();
+
+    /**
+     * @return rotation of the tile
+     */
+    @NotNull Rotation getRotation();
 }
