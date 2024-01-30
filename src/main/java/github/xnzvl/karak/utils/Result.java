3bb2f06ccce9ed2d02915a8af59cf94adfe8cdd5
xnzvl <jakubnezval.jn@gmail.com>
2024-01-31 00:24:30 +0100

    core(utils): Result enumeration

---
+++ src/main/java/github/xnzvl/karak/utils/Result.java
@@ -0,0 +1,11 @@
+package github.xnzvl.karak.utils;
+
+/**
+ * Simple enumeration for binary success/failure situations.
+ *
+ * @author Jakub Nezval
+ */
+public enum Result {
+    SUCCESS,
+    FAILURE;
+}
