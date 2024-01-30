ed294202af0adff37d9c9ce5a4259a033a2d584f
xnzvl <jakubnezval.jn@gmail.com>
2024-01-31 00:25:13 +0100

    core(Hero): public method for PlayerInputHandler

--- src/main/java/github/xnzvl/karak/powerfuls/heroes/Hero.java
+++ src/main/java/github/xnzvl/karak/powerfuls/heroes/Hero.java
@@ -1,17 +1,21 @@
 package github.xnzvl.karak.powerfuls.heroes;
 
 import github.xnzvl.karak.items.Item;
+import github.xnzvl.karak.items.spells.Spell;
+import github.xnzvl.karak.items.weapons.Weapon;
 import github.xnzvl.karak.powerfuls.Power;
 import github.xnzvl.karak.utils.MapUtils;
 import github.xnzvl.karak.utils.Pair;
+import github.xnzvl.karak.utils.Result;
 
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Map;
 
-public abstract class Hero implements Power {
+public class Hero implements Power {
     public static final int MAX_HIT_POINTS = 5;
+    public static final int DEFAULT_NUMBER_OF_STEPS = 4;
 
     private int hitPoints = MAX_HIT_POINTS;
     private Pair<Integer, Integer> position = Pair.of(0, 0);
@@ -22,28 +26,74 @@ public abstract class Hero implements Power {
     @Nullable
     private TurnConstraint turnConstraint = null;
 
-    public Hero() {
+    protected Hero() {
         // nothing so far
     }
 
     public void startTurn() {
-
+        this.turnConstraint = new TurnConstraint(Hero.DEFAULT_NUMBER_OF_STEPS);
     }
 
     public void endTurn() {
+        this.turnConstraint = null;
+    }
+
+    public Result move(
+            Pair<Integer, Integer> newPosition
+    ) {
+        if (!isValidMove(newPosition)) return Result.FAILURE;
+
+        // TODO: change position + remember last position
+
+        return Result.SUCCESS;
+    }
+
+    public Result pickUpItem() {
+
+    }
+
+    public Result pickUpItem(
+            @Nullable Slot slot
+    ) {
+
+    }
+
+    private Result pickUpItem(
+            Slot slot,
+            Weapon weapon
+    ) {
+
+    }
+
+    private Result pickUpItem(
+            Slot slot,
+            Spell spell
+    ) {
+
+    }
+
+    public Result useItem(
+            Slot slot
+    ) {
+
+    }
+
+    public int getPower() {
+
+    }
+
+    public String getTitle() {
+
+    }
+
+    public String getDetails() {
 
     }
 
     protected boolean isValidMove(
-            Pair<Integer, Integer> nextPosition
+            Pair<Integer, Integer> newPosition
     ) {
         return false;
-    }
-
-    protected void pickUpItem(
-            Slot slot
-    ) {
-
     }
 
     protected void recover() {
--- src/main/java/github/xnzvl/karak/powerfuls/heroes/Slot.java
+++ src/main/java/github/xnzvl/karak/powerfuls/heroes/Slot.java
@@ -1,5 +1,7 @@
 package github.xnzvl.karak.powerfuls.heroes;
 
+import java.util.List;
+
 public enum Slot {
     WEAPON_ONE,
     WEAPON_TWO,
@@ -7,4 +9,12 @@ public enum Slot {
     SPELL_TWO,
     SPELL_THREE,
     KEY;
+
+    public List<Slot> getWeaponSlots() {
+        return List.of(WEAPON_ONE, WEAPON_TWO);
+    }
+
+    public List<Slot> getSpellSlots() {
+        return List.of(SPELL_ONE, SPELL_TWO, SPELL_THREE);
+    }
 }
