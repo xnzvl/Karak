03a40e516a1e073beccee70078f02e031bb0c3a8
xnzvl <jakubnezval.jn@gmail.com>
2024-02-25 22:22:51 +0100

    feat(TurnState): additional State

--- src/main/java/github/xnzvl/karak/strengthfuls/heroes/TurnState.java
+++ src/main/java/github/xnzvl/karak/strengthfuls/heroes/TurnState.java
@@ -14,15 +14,15 @@ import org.jetbrains.annotations.Nullable;
 public class TurnState {  // TODO: maybe package private?
     /**
      * Auxiliary enumeration for holding {@link Hero}'s state.
-     * It is better than {@link Boolean} values.
      */
-    private enum HeroState {
+    private enum State {
         WANDERING,
-        FIGHTING;
+        FIGHTING,
+        EXHAUSTED;
     }
 
     private int steps = Hero.DEFAULT_NUMBER_OF_STEPS;
-    private HeroState heroState = HeroState.WANDERING;
+    private State state = State.WANDERING;
     private Pair<Integer, Integer> prevPosition;
 
     public int getSteps() {
@@ -50,44 +50,46 @@ public class TurnState {  // TODO: maybe package private?
      * @return boolean value whether the {@link Hero} is able to move and has steps at the same time
      */
     public boolean canMove() {
-        return this.steps > 0 && this.heroState == HeroState.WANDERING;
+        return this.steps > 0 && this.state == State.WANDERING;
     }
 
     /**
      * Safely subtracts one step.
      */
     public void lowerSteps() {
-        if (this.steps < 1 || this.heroState == HeroState.FIGHTING) {
+        if (this.steps < 1 || this.state == State.FIGHTING) {
             // TODO: throw error
         }
         this.steps -= 1;
     }
 
+    private void setState(
+            TurnState.State state
+    ) {
+        this.state = state;
+    }
+
+    public void setHeroWandering() {
+        this.setState(State.WANDERING);
+    }
+
+    public void setHeroFighting() {
+        this.setState(State.FIGHTING);
+    }
+
+    public void setHeroExhausted() {
+        this.setState(State.EXHAUSTED);
+    }
+
     public boolean isHeroWandering() {
-        return this.heroState == HeroState.WANDERING;
+        return this.state == State.WANDERING;
     }
 
     public boolean isHeroFighting() {
-        return this.heroState == HeroState.FIGHTING;
+        return this.state == State.FIGHTING;
     }
 
-    /**
-     * Sets {@link Hero} into wandering state.
-     * In wandering state {@link Hero} can use only utility spells and can move freely.
-     *
-     * @see TurnState#setHeroFighting()
-     */
-    public void setHeroWandering() {
-        this.heroState = HeroState.WANDERING;
-    }
-
-    /**
-     * Sets {@link Hero} into fighting state.
-     * In fighting state {@link Hero} can use only offensive spells and cannot move until the fight is over.
-     *
-     * @see TurnState#setHeroWandering()
-     */
-    public void setHeroFighting() {
-        this.heroState = HeroState.FIGHTING;
+    public boolean isHeroExhausted() {
+        return this.state == State.EXHAUSTED;
     }
 }
