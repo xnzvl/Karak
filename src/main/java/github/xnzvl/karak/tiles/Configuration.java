cd6e37e09deade3f9de54ed7083ea6ceccc03654
xnzvl <jakubnezval.jn@gmail.com>
2024-02-26 00:20:23 +0100

    feat(Configuration): shortcut sugar enum

---
+++ src/main/java/github/xnzvl/karak/tiles/Configuration.java
@@ -0,0 +1,40 @@
+package github.xnzvl.karak.tiles;
+
+public enum Configuration {
+    ROOM_X (Type.ROOM, Shape.X, null),
+    ROOM_T (Type.ROOM, Shape.T, null),
+    ROOM_L (Type.ROOM, Shape.L, null),
+    ROOM_I (Type.ROOM, Shape.I, null),
+    HALL_X (Type.HALL, Shape.X, null),
+    HALL_T (Type.HALL, Shape.T, null),
+    HALL_L (Type.HALL, Shape.L, null),
+    HALL_I (Type.HALL, Shape.I, null),
+    FOUNTAIN_L (Type.HALL, Shape.L, Feature.FOUNTAIN),
+    PORTAL_I (Type.HALL, Shape.I, Feature.PORTAL);
+
+    private final Type type;
+    private final Shape shape;
+    private final Feature feature;
+
+    Configuration(
+            Type type,
+            Shape shape,
+            Feature feature
+    ) {
+        this.type = type;
+        this.shape = shape;
+        this.feature = feature;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public Feature getFeature() {
+        return feature;
+    }
+}
