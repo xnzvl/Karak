dd3094d8011d6360f91a0344e8002d492007584c
xnzvl <jakubnezval.jn@gmail.com>
2024-03-01 00:27:14 +0100

    feat(Tile): javadocs & minor changes

--- src/main/java/github/xnzvl/karak/board/Tile.java
+++ src/main/java/github/xnzvl/karak/board/Tile.java
@@ -8,39 +8,88 @@ import github.xnzvl.karak.utils.Result;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
+import java.util.Set;
 
 /**
- * Interface that represents a game tile.
+ * Abstract class that represents a game tile.
  *
+ * @see Board
  * @author Jakub Nezval
  */
 public abstract class Tile {
+    /**
+     * Enumeration of features that can a {@link Tile} have.
+     */
     public enum Feature {
         FOUNTAIN,
         PORTAL;
     }
 
+    /**
+     * Enumeration of types that can a {@link Tile} have.
+     */
     public enum Type {
         ROOM,
         HALL;
     }
 
     public abstract Pair<Integer, Integer> getCoordinates();
-    public abstract Shape getShape();
-    public abstract Rotation getRotation();
+
+    /**
+     * Together with {@link Tile.Type} forms "appearance" of the {@link Tile}.
+     * It also dictates where you can go from the {@link Tile}.
+     *
+     * @return shape
+     * @see Tile#getType()
+     * @see Tile.Shape
+     */
+    public abstract Shape getShape();  // TODO: Shape maybe should be an inner class of Tile
+
+    /**
+     * Together with {@link Tile.Type} forms "appearance" of the {@link Tile}.
+     * It also dictates whether a {@link Monster} can spawn on the {@link Tile}.
+     *
+     * @return type
+     * @see Tile#getShape()
+     * @see Tile.Type
+     */
     public abstract Type getType();
 
-    public abstract Result setConditionalRotation(Pair<Integer, Integer> reachableFrom, Rotation rotation);
+    /**
+     * @return how many times the {@link Tile} was shifted clock-wise
+     */
+    public abstract int getNumberOfShifts();
+
+    /**
+     * @return maximum number of clock-wise shifts possible for this {@link Tile}
+     * @see Tile#getNumberOfShifts()
+     */
+    public abstract int getMaxNumberOfShifts();
+
+    /**
+     * @return {@link Set} of coordinates that are directly reachable from this {@link Tile}
+     */
+    public abstract Set<Pair<Integer, Integer>> getAccessibleCoordinates();
+
+    /**
+     * Tries to configure the {@link Tile}. However, the desired `numberOfShifts` might cause an invalid situation.
+     * In that case results with a failure.
+     *
+     * @param reachableFrom coordinates from which the {@link Tile} must be reachable
+     * @param numberOfShifts desired rotation
+     * @return result
+     * @see Result
+     */
+    public abstract Result setConditionalRotation(Pair<Integer, Integer> reachableFrom, int numberOfShifts);
 
     public abstract @Nullable Feature getFeature();
-    public abstract @Nullable Either<Monster, Item> getSubject();
 
+    public abstract @Nullable Either<Monster, Item> getSubject();
     public abstract void setSubject(@Nullable Either<Monster, Item> roomSubject);
 
     /**
-     * @return {@link Collection} of coordinates that are reachable from this {@link Tile}
-     * @see Pair
+     * @return whether the {@link Tile} was rotated - is ready to be put on a {@link Board}
+     * @see Board#placeNewTileAt(Pair, Tile)
      */
-    public abstract Collection<Pair<Integer, Integer>> getAccessibleCoordinates();
+    public abstract boolean isConfigured();
 }
