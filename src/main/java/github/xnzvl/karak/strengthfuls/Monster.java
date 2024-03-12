d82c6999ed793b015cdca7e6b656a8397fd1945d
xnzvl <jakubnezval.jn@gmail.com>
2024-03-13 00:21:52 +0100

    fix: build

--- src/main/java/github/xnzvl/karak/strengthfuls/Monster.java
+++ src/main/java/github/xnzvl/karak/strengthfuls/Monster.java
@@ -4,9 +4,8 @@ import github.xnzvl.karak.description.Description;
 import github.xnzvl.karak.items.Chest;
 import github.xnzvl.karak.items.Item;
 import github.xnzvl.karak.items.Key;
+import github.xnzvl.karak.items.Spell;
 import github.xnzvl.karak.items.Weapon;
-import github.xnzvl.karak.items.spells.impl.FlamingSword;
-import github.xnzvl.karak.items.spells.impl.HealingWarp;
 
 /**
  * Enum for monster units.
@@ -16,9 +15,9 @@ import github.xnzvl.karak.items.spells.impl.HealingWarp;
 public enum Monster implements Description, Strength {
     // TODO: Monster enum descriptions
     RAT                 ("Rat", "", 5, Item.from(Weapon.DAGGER)),
-    SPIDER              ("Spider", "", 6, Item.from(new HealingWarp())),
+    SPIDER              ("Spider", "", 6, Item.from(Spell.HEALING_WARP)),
     SKELETON_KEY_KEEPER ("Skeleton Key Keeper", "", 7, Item.from(new Key())),
-    MUMMY               ("Mummy", "", 7, Item.from(new FlamingSword())),
+    MUMMY               ("Mummy", "", 7, Item.from(Spell.FLAMING_SWORD)),
     SKELETON_SWORDSMAN  ("Skeleton Swordsman", "", 9, Item.from(Weapon.SWORD)),
     SKELETON_KING       ("Skeleton King", "", 10, Item.from(Weapon.AXE)),
     FALLEN              ("Fallen", "", 12, Item.from(Chest.SIMPLE_CHEST_UNLOCKED)),
